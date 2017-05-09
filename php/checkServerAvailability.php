35aec9d0b23e92cf58310017b47552f07e977562
munnaGalal <mona.galal.fci.scu@gmail.com>
2017-05-09 23:30:23 +0200

    check server availability using php

--- php/checkServerAvailability.php
+++ php/checkServerAvailability.php
@@ -1,5 +1,11 @@
 <?php
-	$host = $_GET['name'];
+
+require_once('config.php');
+require_once('DBConnection.php');
+require_once('DBFunctions.php');
+require_once('functions.php');
+
+	$host = $_GET['url'];
 	if($socket =@ fsockopen($host, 80, $errno, $errstr, 30)) {
 	echo 'online';
 	fclose($socket);
