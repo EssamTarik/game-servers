18da3042f7c9d9747cbfad39f416fd5fee438d32
Essam Tarik <essamtarik2000@gmail.com>
2017-05-09 16:07:18 +0200

    search by game name

--- php/DBFunctions.php
+++ php/DBFunctions.php
@@ -10,6 +10,14 @@ function getAllServers(){
 	return $results;
 }
 
+function getGameServers($id){
+	global $pdo;
+	$query = $pdo->prepare('select * from servers where game=?');
+	$query->execute(array($id));
+	$results = $query->fetchAll(PDO::FETCH_ASSOC);
+	return $results;
+}
+
 function getServerInfo($id){
 	global $pdo;
 	$query = $pdo->prepare('select * from servers where id=?');
@@ -20,7 +28,19 @@ function getServerInfo($id){
 
 function addServer($data){
 	global $pdo;
-	$query = $pdo->prepare('insert into servers set name=?, capacity=?, map=?;');
-	$query->execute(array($data->name, $data->capacity, $data->map));
+	$query = $pdo->prepare('insert into servers set name=?, game=?, capacity=?, map=?;');
+	$query->execute(array($data->name, $data->game, $data->capacity, $data->map));
 	return "success";
+}
+
+function findGameByName($name){
+	global $pdo;
+	$query = $pdo->prepare("select * from games where name like ?;");
+	$query->execute(array('%'.$name.'%'));
+	try{
+		$results = $query->fetchAll(PDO::FETCH_ASSOC)[0];
+	}catch(Exception $e){
+		$results = false;
+	}
+	return $results;
 }
---
+++ php/searchByGame.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('config.php');
+require_once('DBConnection.php');
+require_once('DBFunctions.php');
+require_once('functions.php');
+
+$gameInfo = findGameByName($_GET['name']);
+
+if($gameInfo != false){
+	$gameServers =  getGameServers($gameInfo['id']);
+	writeJson(array('info' => $gameInfo, 'servers' => $gameServers));
+}else{
+	writeJson(array('error' => 'game not found'));
+}
